f3d21443771d523fa7c1b2cc1ed1ff9176194057
David <dbadams@iinet.net.au>
2020-12-23 22:12:37 +0800

    adding colorls

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sudo pacman -S --noconfirm tmux vim zsh alacritty neovim
 #install awesome
 sudo pacman -S --noconfirm awesome
 
+
 #install dotfiles
 config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
@@ -39,6 +40,10 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ## vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+## colorls
+sudo pacman -S ruby
+gem install colorls
 
 # install fonts
 git clone https://aur.archlinux.org/packages/ttf-meslo-nerd-font-powerlevel10k/
