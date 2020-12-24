77c6b73dd34af8583ee0e37bcaa3d891a532c3d6
dadams2 <dbadams@iinet.net.au>
2020-12-24 16:50:46 +0800

    adding zsh syntax highlighting

--- install.sh
+++ install.sh
@@ -40,6 +40,8 @@ cd autojump
 makepkg -si
 cd ..
 
+## zsh syntax highilghting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 #install dotfiles
 config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
