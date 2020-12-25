b22977e6f87a776ac6f461c870cc4f3ec635c320
dadams2 <dbadams@iinet.net.au>
2020-12-25 20:57:28 +0800

    tmux issue again

--- install.sh
+++ install.sh
@@ -29,8 +29,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 ## Tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux &
 
+# making sure tmux exists
+tmux new -s default
 ## vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
