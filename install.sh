7094c4009389f99a5c5bc6080144c6c3047d38ab
dadams2 <dbadams@iinet.net.au>
2021-02-05 09:50:31 +0800

    adding proper installation

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 
 
 # install oh my bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
 
 # install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
 
 source .bashrc
