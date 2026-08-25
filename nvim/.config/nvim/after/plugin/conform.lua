7c6d6f0856da8f2aad8c52d15d0b583f529b491c
David Adams <git@dadams2.com>
2026-08-25 20:07:04 +1000

    add formatting

---
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -0,0 +1,34 @@
+local conform = require("conform")
+
+conform.setup({
+    formatters_by_ft = {
+        javascript = { "prettier", "prettierd", stop_after_first = true },
+        typescript = { "prettier", "prettierd", stop_after_first = true },
+        typescriptreact = { "prettier", "prettierd", stop_after_first = true },
+        python = { "autopep8", stop_after_first = true }
+    }
+})
+
+local formatOnSave = false
+
+vim.keymap.set("n", "<leader>wr", function()
+    formatOnSave = not formatOnSave
+    print("Formatting on Save: " .. tostring(formatOnSave))
+end)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        if formatOnSave then
+            conform.format({ timeout_ms = 300, bufnr = args.buf, lsp_format = "fallback" })
+        end
+    end
+})
+
+
+-- format whenevery
+vim.keymap.set("n", "<leader>wt", function()
+    print("Formatting Buffer")
+    conform.format({ async = true, lsp_format = "fallback" })
+end)
+
