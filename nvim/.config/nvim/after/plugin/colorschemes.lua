02f86528498b3d9ddf034fe929bd579c9db6408b
David Adams <git@dadams2.com>
2026-08-25 18:55:42 +1000

    add simple nord colorscheme to nvim

---
+++ nvim/.config/nvim/after/plugin/colorschemes.lua
@@ -0,0 +1,53 @@
+-- Holds all the colour schemes (The ones to rotate through)
+
+local colourSchemes = {
+    "nord",
+    -- "kanagawa",
+    -- "tokyonight",
+    -- "ashen",
+    -- "onedark",
+    -- "evergarden",
+    -- "catppuccin",
+    -- "elflord",
+    -- "edge"
+}
+
+local currentColour = 1
+local transparent = false
+
+local function setIndentColour()
+    vim.cmd.highlight('IndentLine guifg=#4c4b59') -- Sets the indent colour (unselected)
+end
+
+local function makeTransparent()
+    -- Removed "NormalFloat" from the list to keep highlighting
+    local groups = { "Normal", "FloatBorder", "Pmenu" }
+    for _, group in ipairs(groups) do
+        vim.api.nvim_set_hl(0, group, { bg = 'NONE' })
+    end
+end
+
+-- Initial Setup
+vim.cmd([[colorscheme ]] .. colourSchemes[currentColour])
+setIndentColour()
+
+vim.keymap.set("n", "<leader>n", function()
+    currentColour = (currentColour) % #colourSchemes + 1
+    vim.cmd("colorscheme " .. colourSchemes[currentColour])
+    if transparent then
+        makeTransparent()
+    end
+    setIndentColour()
+end)
+
+
+vim.keymap.set("n", "<leader><leader>n", function()
+    if not transparent then
+        makeTransparent()
+        setIndentColour()
+    else
+        vim.cmd("colorscheme " .. colourSchemes[currentColour])
+    end
+    transparent = not transparent
+end)
+
