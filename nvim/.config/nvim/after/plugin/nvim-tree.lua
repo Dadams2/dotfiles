b4129eee480a971b8b2b9f06c5b9d9c4c6c1e2b2
David Adams <git@dadams2.com>
2026-08-25 21:22:15 +1000

    format nvimtree

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -8,40 +8,40 @@ vim.opt.termguicolors = true
 -- OR setup with some options
 require("nvim-tree").setup({
 
-    sync_root_with_cwd = true,
+	sync_root_with_cwd = true,
 
-    update_focused_file = {
-        enable = true,
-        update_root = {
-            enable = true,
-        },
-    },
+	update_focused_file = {
+		enable = true,
+		update_root = {
+			enable = true,
+		},
+	},
 
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 30,
-    },
-    renderer = {
-        group_empty = true,
-        icons = {
-            show = {
-                file = true,
-                folder = true,
-                folder_arrow = true,
-                git = true,
-                modified = true,
-                hidden = true,
-                diagnostics = true,
-                bookmarks = true,
-            },
-        },
-    },
-    filters = {
-        dotfiles = false,
-    },
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+				modified = true,
+				hidden = true,
+				diagnostics = true,
+				bookmarks = true,
+			},
+		},
+	},
+	filters = {
+		dotfiles = false,
+	},
 })
 
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader><leader>t", ":NvimTreeCollapse<CR>")
+-- vim.keymap.set("n", "<leader><leader>t", ":NvimTreeCollapse<CR>")
